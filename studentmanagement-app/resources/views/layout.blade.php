612d59893d0ede177c500422649d008c2795f193
yesinidulij <yesinidulij123@gmail.com>
2024-08-21 06:56:34 -0700

    student application

---
+++ studentmanagement-app/resources/views/layout.blade.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student Management System</title>
+<style>
+    /* The side navigation menu */
+.sidebar {
+  margin: 0;
+  padding: 0;
+  width: 200px;
+  background-color: #f1f1f1;
+  position: fixed;
+  height: 100%;
+  overflow: auto;
+}
+
+/* Sidebar links */
+.sidebar a {
+  display: block;
+  color: black;
+  padding: 16px;
+  text-decoration: none;
+}
+
+/* Active/current link */
+.sidebar a.active {
+  background-color: #04AA6D;
+  color: white;
+}
+
+/* Links on mouse-over */
+.sidebar a:hover:not(.active) {
+  background-color: #555;
+  color: white;
+}
+
+/* Page content. The value of the margin-left property should match the value of the sidebar's width property */
+div.content {
+  margin-left: 200px;
+  padding: 1px 16px;
+  height: 1000px;
+}
+
+/* On screens that are less than 700px wide, make the sidebar into a topbar */
+@media screen and (max-width: 700px) {
+  .sidebar {
+    width: 100%;
+    height: auto;
+    position: relative;
+  }
+  .sidebar a {float: left;}
+  div.content {margin-left: 0;}
+}
+
+/* On screens that are less than 400px, display the bar vertically, instead of horizontally */
+@media screen and (max-width: 400px) {
+  .sidebar a {
+    text-align: center;
+    float: none;
+  }
+}
+</style>
+</head>
+<body>
+    <div class="container">
+     <div class="row">
+    <div class="col-md-12">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+ <h1> <a class="navbar-brand" href="#">Student Management System</a></h1>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      
+  </div>
+</nav>
+</div>
+</div>
+<div class="row">
+    <div class="col-md-3">
+    <div class="sidebar">
+  <a class="active" href="#home">Home</a>
+  <a href="#news">Student</a>
+  <a href="#contact">Teacher</a>
+  <a href="#about">Courses</a>
+  <a href="#about">Enrollment</a>
+  <a href="#about">Payment</a>
+
+</div>
+    </div>
+    <div class="col-md-9">
+    @yield('content')
+    </div>
+</div>
+    </div>
+</body>
+</html>
---
+++ studentmanagement-app/resources/views/students/index.blade.php
@@ -0,0 +1,113 @@
+@extends('layout')
+
+@section('content')
+
+    <div class="container">
+
+        <h3 align="center" class="mt-5">Student Application</h3>
+
+        <div class="row">
+            <div class="col-md-2">
+            </div>
+            <div class="col-md-8">
+
+            <div class="form-area">
+                <form method="POST" action="{{ route('students.store') }}">
+                    @csrf
+                    <div class="row">
+                        <div class="col-md-6">
+                            <label>Student Name</label>
+                            <input type="text" class="form-control" name="emp_name">
+                        </div>
+                        <div class="col-md-6">
+                            <label>Student DOB</label>
+                            <input type="date" class="form-control" name="dob">
+
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <label>Phone</label>
+                            <input type="text" class="form-control" name="phone">
+
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12 mt-3">
+                            <input type="submit" class="btn btn-info" value="Register">
+                        </div>
+
+                    </div>
+                </form>
+            </div>
+
+                <table class="table mt-5">
+                    <thead>
+                      <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Student Name</th>
+                        <th scope="col">DOB</th>
+                        <th scope="col">Mobile</th>
+                        <th scope="col">Action</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+
+                        @foreach ( $students as $key => $student )
+
+                        <tr>
+                            <td scope="col">{{ ++$key }}</td>
+                            <td scope="col">{{ $student->name }}</td>
+                            <td scope="col">{{ $student->address }}</td>
+                            <td scope="col">{{ $student->mobile }}</td>
+                            <td scope="col">
+
+                            <a href="{{  route('employee.edit', $employee->id) }}">
+                            <button class="btn btn-primary btn-sm">
+                            <i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit
+                            </button>
+                            </a>
+                            
+                            <form action="{{ route('employee.destroy', $employee->id) }}" method="POST" style ="display:inline">
+                             @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger btn-sm">Delete</button>
+                            </form>
+                            </td>
+
+                          </tr>
+
+                        @endforeach
+
+
+
+
+                    </tbody>
+                  </table>
+            </div>
+        </div>
+    </div>
+
+@endsection
+
+
+@push('css')
+    <style>
+        .form-area{
+            padding: 20px;
+            margin-top: 20px;
+              background-color:#FFFF00;
+        }
+
+        .bi-trash-fill{
+            color:red;
+            font-size: 18px;
+        }
+
+        .bi-pencil{
+            color:green;
+            font-size: 18px;
+            margin-left: 20px;
+        }
+    </style>
+@endpush
